fa89714ad1ddb4d930c08ba918fc56d7ee441d4b
dckristiono <dckristiono@gmail.com>
2018-04-26 09:23:13 +0700

    fiixx

--- src/services/absencestypesmanagement/absencestypesmanagement.service.js
+++ src/services/absencestypesmanagement/absencestypesmanagement.service.js
@@ -5,7 +5,7 @@ module.exports = function () {
   const app = this;
 
   app.disable('etag');
-  app.use('/absencestypesmanagement', new AbsencesTypesManagement());
+  app.use('/absencestypesmanagement', new absencestypesmanagement());
   // Get our initialized service so that we can register hooks and filters
   const service = app.service('absencestypesmanagement');
 
