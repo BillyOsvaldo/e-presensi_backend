a976049e7c75e4df6a18a0d37140f25ea4e99f5d
f4th4n <wilfathan@gmail.com>
2018-04-28 13:36:11 +0700

    Service find-users-by-organization find: add fullname

--- src/services/find-users-by-organization/find-users-by-organization.class.js
+++ src/services/find-users-by-organization/find-users-by-organization.class.js
@@ -5,14 +5,38 @@ class Service {
   }
 
   async find (params) {
+
+    const profiles = params.client.service('profiles')
+
     var params2 = params
     params2.query = {
       organization: params.query.organization,
       $nopaginate: false,
-      $select: [ '_id' ]
+      $select: [ '_id', 'profile' ]
     }
 
-    const docs = await params.client.service('users').find(params2)
+    var docs = await params.client.service('users').find(params2)
+
+    for(let doc of docs) {
+      if(!doc.profile) {
+        doc.name = ''
+        continue
+      }
+
+      let docProfile = await profiles.get(doc.profile)
+      let name = docProfile.name
+
+      var firstTitle
+      if(name.first_title) {
+        firstTitle = ' ' + name.first_title
+      } else {
+        firstTitle = ''
+      }
+
+      let fullname = `${firstTitle}${name.first_name} ${name.last_name} ${name.last_title} `
+      doc.name = fullname
+    }
+
     const result = {
       total : docs.length,
       limit : docs.length,
