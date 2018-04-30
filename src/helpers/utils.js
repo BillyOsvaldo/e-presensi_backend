274fb243d79f5a196ba59b2b7b6c2762cdfe980f
f4th4n <wilfathan@gmail.com>
2018-04-30 10:33:29 +0700

    Add helper getFullName()

---
+++ src/helpers/utils.js
@@ -0,0 +1,20 @@
+module.exports.getFullName = (docProfile) => {
+  let name = docProfile.name
+
+  var firstTitle
+  if(name.first_title) {
+    firstTitle = name.first_title + '. '
+  } else {
+    firstTitle = ''
+  }
+
+  var lastTitle
+  if(name.last_title) {
+    lastTitle = ' ' + name.last_title
+  } else {
+    lastTitle = ''
+  }
+
+  let fullname = `${firstTitle}${name.first_name} ${name.last_name}${lastTitle}`
+  return fullname
+}
