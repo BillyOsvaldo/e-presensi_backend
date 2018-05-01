4fba028fe686d93bc7cb0663c52b4b5485e09904
f4th4n <wilfathan@gmail.com>
2018-05-01 23:36:57 +0700

    Update

--- src/hooks/emit_create_presence.js
+++ src/hooks/emit_create_presence.js
@@ -5,7 +5,8 @@ module.exports = (context) => {
     _id: context.result._id,
     name: context.params.name,
     time: context.data.time,
-    mode: context.data.mode
+    user: context.data.user,
+    mode: parseInt(context.data.mode)
   }
 
   context.service.emit(eventName, data)
