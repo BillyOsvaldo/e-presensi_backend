4e5c50e3742bbd04d56aea1ba078f1343ff7eb99
f4th4n <wilfathan@gmail.com>
2018-05-01 22:57:10 +0700

    Presences create: add _id on event

--- src/hooks/emit_create_presence.js
+++ src/hooks/emit_create_presence.js
@@ -2,10 +2,12 @@ module.exports = (context) => {
   const eventName = 'organization_' + context.params.organization
 
   const data = {
+    _id: context.result._id,
     name: context.params.name,
     time: context.data.time,
     mode: context.data.mode
   }
 
+  console.log('context.result._id', context.result._id)
   context.service.emit(eventName, data)
 }
