c3a124fdc86d862c27b69bbb830cc3ed6a15c0af
f4th4n <wilfathan@gmail.com>
2018-07-25 10:22:27 +0700

    Fix tz

--- src/hooks/update_machine_is_match.js
+++ src/hooks/update_machine_is_match.js
@@ -1,13 +1,17 @@
+const moment = require('moment')
+
 // fill is_match
 module.exports = async (context) => {
   // assumed fk_time is timestamp (in seconds)
   const fkTime = context.data.fk_time
-  const currentTimestamp = Math.floor((+ new Date())/1000)
-  const diff = Math.abs(fkTime - currentTimestamp)
+  const fkTimeTimestampJakarta = moment(fkTime, 'YYYYMMDDHHmmss').unix()
+  const fkTimeTimestampGlobal = fkTimeTimestampJakarta - (7 * 60 * 60)
+  const currentTimestamp = Math.floor((+ new Date()) / 1000)
+  const diff = Math.abs(fkTimeTimestampGlobal - currentTimestamp)
 
   context.data.is_match = Boolean(diff <= 3)
   console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-  console.log('fkTime', fkTime)
+  console.log('fkTimeTimestampGlobal', fkTimeTimestampGlobal)
   console.log('currentTimestamp', currentTimestamp)
   console.log('diff', diff)
   console.log('context.data.is_match', context.data.is_match)
