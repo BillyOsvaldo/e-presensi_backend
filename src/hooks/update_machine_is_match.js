1c6850acfe7bc279d3d3b6f0162bade06b6c93e7
f4th4n <wilfathan@gmail.com>
2018-07-30 14:08:23 +0700

    Update machine.is_match: fix bug

--- src/hooks/update_machine_is_match.js
+++ src/hooks/update_machine_is_match.js
@@ -3,6 +3,7 @@ const moment = require('moment')
 // fill is_match
 module.exports = async (context) => {
   const Settings = context.app.service('settings').Model
+  console.log('----------------------------------')
 
   // assumed fk_time is timestamp (in seconds)
   const fkTime = context.data.fk_time
@@ -15,5 +16,19 @@ module.exports = async (context) => {
   console.log('setting_interval', interval)
   console.log('diff between machine and server', diff)
 
-  context.data.is_match = Boolean(diff <= interval)
+  const isMatch = Boolean(diff <= interval)
+  console.log('isMatch', isMatch)
+  if(isMatch) {
+    context.data.is_match = isMatch
+  } else {
+    const docMachine = await context.app.service('machines').get(context.id)
+    const currentFkTime = docMachine.fk_time
+    console.log('after offline')
+    console.log('currentFkTime', currentFkTime)
+    console.log('context.data.fk_time', context.data.fk_time)
+    const isMatch2 = currentFkTime == context.data.fk_time
+    context.data.is_match = isMatch2
+  }
+
+  console.log('context.data.is_match', context.data.is_match)
 }
