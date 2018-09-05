3bed4cea945307dd2c9680f405160914f267eb13
f4th4n <wilfathan@gmail.com>
2018-09-05 09:23:08 +0700

    Remove unused code

--- src/hooks/machines_users.js
+++ src/hooks/machines_users.js
@@ -6,7 +6,7 @@ module.exports.ignoreIfExist = async (context) => {
   const where = { 'user._id': objectid(context.data.user._id) }
   var doc = await MachinesUsers.findOne(where)
   const isExist = Boolean(doc)
-  console.log('isExist', isExist)
+
   if(isExist) {
     doc = JSON.parse(JSON.stringify(doc))
     doc.ignored = true
